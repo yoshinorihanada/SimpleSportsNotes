99c2dcf339c875e9d615444c818ebaecffb0ba78
yoshinorihanada <yoshihana747@gmail.com>
2024-10-31 20:05:22 +0900

    Initial Commit

---
+++ SimpleSportsNotes/SimpleSportsNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleSportsNotesApp.swift
+//  SimpleSportsNotes
+//
+//  Created by Yoshinori Hanada on 2024/10/31.
+//
+
+import SwiftUI
+
+@main
+struct SimpleSportsNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
